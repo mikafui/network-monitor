b13124c1ab9ad2526977af5352679f9dca58fe72
Mika Fuisting <mika.fuisting@gmx.de>
2026-08-08 12:25:54 +0200

    fix(lint): update lint-staged configuration for TypeScript and styles

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,5 +1,5 @@
 export default {
-  '*.{ts,tsx}': ['eslint --flag unstable_native_nodejs_ts_config .'],
-  '*.{pcss,css,scss}': ['npm run lint:style'],
-  '*.cs': ['npm run lint:csharp']
+  '*.ts': ['eslint --flag unstable_native_nodejs_ts_config'],
+  '*.html': ['eslint --flag unstable_native_nodejs_ts_config'],
+  '*.{scss,css}': ["stylelint '*.{css,scss}'"]
 };
