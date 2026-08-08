a781d68d1e2c6040d80e1723225e79dc793114e9
Mika Fuisting <mika.fuisting@gmx.de>
2026-08-08 14:59:46 +0200

    feat(display): initialize display package with Electron and TypeScript setup

---
+++ packages/display/src/main.ts
@@ -0,0 +1,31 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1000,
+    height: 700,
+
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  window.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
